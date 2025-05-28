b9f641ec343f8adac7d47b50e00598f3b402b422
Maryam <maryam.alimadadi@yh.nackademin.se>
2025-05-28 22:35:06 +0200

    Added failling test for pipeline verification

--- calculateTests.py
+++ calculateTests.py
@@ -1,46 +1,33 @@
 import unittest
-
 from calculate import canIBuyBeer
-
 
 class TestBeer(unittest.TestCase):
     def test_when_17_and_on_krogen_should_not_be_allowed(self):
-        # ARRANGE
         age = 17
         loc = "krogen"
-        # ACT
-        c = canIBuyBeer(age,loc)
-        # ASSERT
-        #self.assertGreater(990)
+        c = canIBuyBeer(age, loc)
         self.assertFalse(c)
 
     def test_when_18_and_on_krogen_should_be_allowed(self):
-        # ARRANGE
         age = 18
         loc = "krogen"
-        # ACT
-        c = canIBuyBeer(age,loc)
-        # ASSERT
+        c = canIBuyBeer(age, loc)
         self.assertTrue(c)
 
-
     def test_when_20_and_on_systemet_should_be_allowed(self):
-        # ARRANGE
         age = 20
         loc = "systemet"
-        # ACT
-        c = canIBuyBeer(age,loc)
-        # ASSERT
+        c = canIBuyBeer(age, loc)
         self.assertTrue(c)
 
     def test_when_19_and_on_systemet_should_not_be_allowed(self):
-        # ARRANGE
-        age =19
+        age = 19
         loc = "systemet"
-        # ACT
-        c = canIBuyBeer(age,loc)
-        # ASSERT
+        c = canIBuyBeer(age, loc)
         self.assertFalse(c)
 
+    def test_always_fails(self):
+        self.assertTrue(False, "This test is designed to fail")
+
 if __name__ == '__main__':
-    unittest.main()        
+    unittest.main()
